27405fe30006aae729a6c07ab61a9c6d71f102fb
dunia-and-the-code <dunia-al-baghdadi@hotmail.com>
2019-03-25 13:06:52 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect(array)
+  if block_given?
+    i = 0
+    new_array = []
 
+    while i < array.length
+      new_array << yield(array[i])
+      i = i + 1
 
+    end
+      new_array
+  end
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
