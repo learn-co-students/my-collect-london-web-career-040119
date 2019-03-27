c5c9f9a37afb48941d9a6b8d8759620cc0b18edc
harriet.ballantyne <harriet.ballantyne@gmail.com>
2019-03-27 18:44:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(array)
+  if array.length == 0
+    "Empty"
+  else
+  i = 0
+  new_array = []
+    while (i < array.length)
+    lang = array[i]
+    new_array.push(yield(lang))
+    i += 1 
+    end
+    new_array
+  end
+end
 
+array = ["ruby", "javascript", "python", "objective-c"]
+
+my_collect(array) do |lang|
+  lang.upcase
+end
 
