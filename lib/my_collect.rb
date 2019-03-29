26e63d06586e9f2c2ad9e7f756b3b4eac264449c
Ay Has <ahashim983@yahoo.com>
2019-03-29 01:38:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(collection)
+  new_collection = Array.new
+  if block_given?
+    i = 0
+    while i < collection.length
+        new_collection.push(yield collection[i])
+      i += 1
+    end
+  else
+    return "No block was given!"
+  end
+  return new_collection
+end
 
-
+my_collect(["a","b","c"]) do |item|
+  return item.upcase
+end
